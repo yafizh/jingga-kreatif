9bdcd36d3b62a743fe171869d6a5b6d93ec4e21d
yafizh <a.chairil.yafizh@gmail.com>
2022-09-17 17:15:10 +0800

    custome primary color bootstrap 5.2

--- resources/views/landing/layout/main.blade.php
+++ resources/views/landing/layout/main.blade.php
@@ -5,8 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Jingga Kreatif</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/main.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
     <link rel="icon" type="image/png" href="/images/favicon.png" />
 </head>
