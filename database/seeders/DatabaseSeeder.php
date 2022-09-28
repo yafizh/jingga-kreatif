b6dbba9e775fac89610b82886dac79b8dc930820
yafizh <a.chairil.yafizh@gmail.com>
2022-09-28 09:14:13 +0800

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Vendor;
 use App\Models\VendorType;
 use Illuminate\Database\Seeder;
 
@@ -11,29 +12,126 @@ class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        VendorType::create([
+        $dekorasi = VendorType::create([
             'name' => 'Dekorasi',
             'description' => ''
-        ]);
-        VendorType::create([
+        ])->id;
+        $dokumentasi = VendorType::create([
             'name' => 'Dokumentasi',
             'description' => ''
-        ]);
-        VendorType::create([
+        ])->id;
+        $lighting = VendorType::create([
             'name' => 'Lighting',
             'description' => ''
-        ]);
-        VendorType::create([
+        ])->id;
+        $band = VendorType::create([
             'name' => 'Band',
             'description' => ''
-        ]);
-        VendorType::create([
+        ])->id;
+        $mc = VendorType::create([
             'name' => 'MC',
             'description' => ''
-        ]);
-        VendorType::create([
+        ])->id;
+        $mua = VendorType::create([
             'name' => 'MUA',
             'description' => ''
+        ])->id;
+        $sound = VendorType::create([
+            'name' => 'Sound',
+            'description' => ''
+        ])->id;
+
+        // Vendor Dekorasi
+        Vendor::create([
+            "vendor_type_id" => $dekorasi,
+            "name" => "CLOVER",
+            "price" => 45000000,
+            "logo" => "vendor-logo/clover.jpg",
+            "description" => "",
         ]);
+        Vendor::create([
+            "vendor_type_id" => $dekorasi,
+            "name" => "Galathia Decor",
+            "price" => 50000000,
+            "logo" => "vendor-logo/galathia.jpg",
+            "description" => "",
+        ]);
+        Vendor::create([
+            "vendor_type_id" => $dekorasi,
+            "name" => "Cassia Decoration",
+            "price" => 17000000,
+            "logo" => "vendor-logo/cassia.jpg",
+            "description" => "",
+        ]);
+
+        // Vendor Dokumentasi
+        Vendor::create([
+            "vendor_type_id" => $dokumentasi,
+            "name" => "Lemia Project",
+            "price" => 22000000,
+            "logo" => "vendor-logo/lemia.png",
+            "description" => "",
+        ]);
+        Vendor::create([
+            "vendor_type_id" => $dokumentasi,
+            "name" => "Buana&Co",
+            "price" => 24000000,
+            "logo" => "vendor-logo/buana.jpg",
+            "description" => "",
+        ]);
+        Vendor::create([
+            "vendor_type_id" => $dokumentasi,
+            "name" => "Hope Portraiture",
+            "price" => 20000000,
+            "logo" => "vendor-logo/hope.png",
+            "description" => "",
+        ]);
+
+        // Vendor Sound
+        Vendor::create([
+            "vendor_type_id" => $sound,
+            "name" => "JPMedia",
+            "price" => 18000000,
+            "logo" => "vendor-logo/jpmedia.jfif",
+            "description" => "",
+        ]);
+        Vendor::create([
+            "vendor_type_id" => $sound,
+            "name" => "Twinbrother Production",
+            "price" => 15000000,
+            "logo" => "vendor-logo/twin.jfif",
+            "description" => "",
+        ]);
+        Vendor::create([
+            "vendor_type_id" => $sound,
+            "name" => "Mega Sound Sytem",
+            "price" => 16000000,
+            "logo" => "vendor-logo/mega.jfif",
+            "description" => "",
+        ]);
+
+        // Lighting Sound
+        Vendor::create([
+            "vendor_type_id" => $lighting,
+            "name" => "LUMENS",
+            "price" => 15000000,
+            "logo" => "vendor-logo/lumen.png",
+            "description" => "",
+        ]);
+        Vendor::create([
+            "vendor_type_id" => $lighting,
+            "name" => "Lightworks",
+            "price" => 25000000,
+            "logo" => "vendor-logo/Lightworks.webp",
+            "description" => "",
+        ]);
+        Vendor::create([
+            "vendor_type_id" => $lighting,
+            "name" => "Lasika Production",
+            "price" => 25000000,
+            "logo" => "vendor-logo/lasika.png",
+            "description" => "",
+        ]);
+
     }
 }
