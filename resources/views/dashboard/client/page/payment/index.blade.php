4c1b957e4c19988a7d06636e3903be5e965a6a2d
yafizh <a.chairil.yafizh@gmail.com>
2022-09-28 09:15:31 +0800

    change UI of payment feature

--- resources/views/dashboard/client/page/payment/index.blade.php
+++ resources/views/dashboard/client/page/payment/index.blade.php
@@ -9,7 +9,7 @@
         <div class="col-12">
             <div class="mb-3 p-4 bg-white jingga-shadow rounded-2 w-100">
                 <div class="row p-4 text-center">
-                    <div class="col-12 col-md-6" style="border-right: 1px solid black;">
+                    <div class="col-12 col-md-6">
                         <h2 class="mb-3">Rp. 5.000.000</h2>
                         <h5 class="text-muted">Perlu Dibayar</h5>
                     </div>
@@ -19,11 +19,14 @@
                     </div>
                 </div>
                 <hr>
-                <div class="row align-items-center">
+                {{-- <div class="row align-items-center">
                     <div class="col-12 text-center col-lg-3 my-1 align-self-center">Pembayaran Vendor</div>
                     <div class="col-12 text-center col-lg-3 my-1 align-self-center">Rp. 5.000.000</div>
                     <div class="col-12 text-center col-lg-3 my-1 align-self-center"><span class="rounded-pill text-white px-3 py-1 bg-warning">Menuggu Pembayaran</span></div>
                     <div class="col-12 text-center col-lg-3 my-1 align-self-center"><button class="btn btn btn-primary">Lakukan Pembayaran</button></div>
+                </div> --}}
+                <div class="row align-items-center">
+                    <h5 class="text-center text-muted mb-0">Tidak Ada Pembayaran</h5>
                 </div>
                 <hr>
             </div>
