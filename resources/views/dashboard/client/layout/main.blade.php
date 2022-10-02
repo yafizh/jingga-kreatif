16199a8f81726c613ba9a789f12f0dc483e0b038
yafizh <a.chairil.yafizh@gmail.com>
2022-10-02 08:49:45 +0800

    set temporary icon

--- resources/views/dashboard/client/layout/main.blade.php
+++ resources/views/dashboard/client/layout/main.blade.php
@@ -16,6 +16,7 @@
     <link rel="stylesheet" href="/styles/sidebar.css">
     <link rel="stylesheet" href="/styles/mobile.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link rel="icon" type="image/png" href="/images/favicon.png" />
 </head>
 
 <body>
