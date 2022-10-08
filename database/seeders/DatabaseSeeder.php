a98be8fb0f14ae4a683fa71d4e04ee4028a18a8d
yafizh <a.chairil.yafizh@gmail.com>
2022-10-09 05:53:55 +0800

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ namespace Database\Seeders;
 
 use App\Models\Bank;
 use App\Models\Client;
+use App\Models\Employee;
 use App\Models\MeetingHistory;
 use App\Models\Newlywed;
 use App\Models\Payment;
@@ -28,89 +29,89 @@ class DatabaseSeeder extends Seeder
             'password' => bcrypt('admin'),
         ]);
 
-        User::create([
-            'email' => 'jinggakreatif93@gmail.com',
-            'password' => bcrypt('jinggakreatif93@gmail.com'),
-        ]);
+        // User::create([
+        //     'email' => 'jinggakreatif93@gmail.com',
+        //     'password' => bcrypt('jinggakreatif93@gmail.com'),
+        // ]);
 
-        Client::create([
-            'user_id' => 2,
-            'name' => 'Nurcholis',
-            'phone_number' => '098766222322',
-            'email' => 'jinggakreatif93@gmail.com',
-        ]);
+        // Client::create([
+        //     'user_id' => 2,
+        //     'name' => 'Nurcholis',
+        //     'phone_number' => '098766222322',
+        //     'email' => 'jinggakreatif93@gmail.com',
+        // ]);
 
-        Wedding::create([
-            'client_id' => 1
-        ]);
+        // Wedding::create([
+        //     'client_id' => 1
+        // ]);
 
-        Newlywed::create([
-            'wedding_id' => 1,
-            'nik' => '3525015201880002',
-            'name' => 'Muhammad Putra',
-            'birthplace' => 'Martapura',
-            'birthdate' => '2000-01-01',
-            'sex' => true,
-            'father_name' => 'Putra',
-            'mother_name' => 'Putri',
-            'photo' => '',
-        ]);
+        // Newlywed::create([
+        //     'wedding_id' => 1,
+        //     'nik' => '3525015201880002',
+        //     'name' => 'Muhammad Putra',
+        //     'birthplace' => 'Martapura',
+        //     'birthdate' => '2000-01-01',
+        //     'sex' => true,
+        //     'father_name' => 'Putra',
+        //     'mother_name' => 'Putri',
+        //     'photo' => '',
+        // ]);
 
-        Newlywed::create([
-            'wedding_id' => 1,
-            'nik' => '3525016005650004',
-            'name' => 'Just Putri',
-            'birthplace' => 'Banjarbaru',
-            'birthdate' => '2000-05-05',
-            'sex' => false,
-            'father_name' => 'Putra 2',
-            'mother_name' => 'Putri 2',
-            'photo' => '',
-        ]);
+        // Newlywed::create([
+        //     'wedding_id' => 1,
+        //     'nik' => '3525016005650004',
+        //     'name' => 'Just Putri',
+        //     'birthplace' => 'Banjarbaru',
+        //     'birthdate' => '2000-05-05',
+        //     'sex' => false,
+        //     'father_name' => 'Putra 2',
+        //     'mother_name' => 'Putri 2',
+        //     'photo' => '',
+        // ]);
 
-        MeetingHistory::create([
-            'wedding_id' => 1,
-            'topic' => 'Pertemuan 1',
-            'meeting_date' => Carbon::now(),
-            'meeting_time' => Carbon::now(),
-            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae optio odio cum asperiores rem delectus aut laborum maxime culpa sunt. Exercitationem voluptatem praesentium nesciunt accusamus fugit explicabo laboriosam dolorem autem!',
-            'photo' => 'meeting-photo/cafe1.avif'
-        ]);
+        // MeetingHistory::create([
+        //     'wedding_id' => 1,
+        //     'topic' => 'Pertemuan 1',
+        //     'meeting_date' => Carbon::now(),
+        //     'meeting_time' => Carbon::now(),
+        //     'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae optio odio cum asperiores rem delectus aut laborum maxime culpa sunt. Exercitationem voluptatem praesentium nesciunt accusamus fugit explicabo laboriosam dolorem autem!',
+        //     'photo' => 'meeting-photo/cafe1.avif'
+        // ]);
 
-        MeetingHistory::create([
-            'wedding_id' => 1,
-            'topic' => 'Pertemuan 2',
-            'meeting_date' => Carbon::now(),
-            'meeting_time' => Carbon::now(),
-            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus ipsum, quisquam neque nobis molestiae quam qui numquam. Possimus harum nostrum iste architecto cumque distinctio, officia iure placeat minus et voluptas?
-            Iusto doloremque, ab nisi, sunt ipsam quidem sit laboriosam itaque hic neque tempora commodi facilis? Dolore nesciunt saepe maxime odio delectus? Accusantium nostrum libero odit, ea eaque earum necessitatibus rerum?
-            Odit iusto, voluptatem aspernatur ducimus reprehenderit aliquam harum quasi porro officia omnis nulla ut necessitatibus ea beatae consequuntur, quos, a sapiente cumque. Eos, laborum repellat praesentium impedit dolore dignissimos nemo.
-            Exercitationem repudiandae officia reprehenderit tenetur maiores fugit aliquam, quae quibusdam quod impedit enim sequi nostrum debitis, dolorem provident asperiores deleniti laboriosam. Similique quia saepe vel nam earum explicabo sed aperiam.
-            Quam rem minima fugiat. Facere inventore voluptate dolorum eius architecto nihil nulla iusto hic perferendis? Incidunt, ab illo! Ad libero quam voluptatem deserunt amet quo reprehenderit et molestiae autem suscipit?
-            Quam labore voluptatem illum illo consequuntur nesciunt eligendi quidem obcaecati praesentium nihil deleniti aliquam, eum ipsam neque beatae quia laborum dolores facilis. Enim laudantium nihil atque voluptatum mollitia libero aliquam.
-            Sapiente quaerat rerum nesciunt deserunt odit repudiandae minus, quos praesentium itaque ad laudantium corrupti nostrum expedita eveniet dolorum cumque fugiat reiciendis voluptate eligendi nemo doloremque earum ipsum. Voluptate, possimus nesciunt.
-            Doloremque deleniti beatae quae tempore aspernatur facere praesentium harum fugiat corporis eligendi dolores dolore provident corrupti nobis inventore non quo molestias ab eos, qui vel laborum debitis. Eaque, cupiditate nostrum.
-            Sint perspiciatis maiores iste sit harum eos voluptates, suscipit nesciunt tempora, ut nihil magni beatae dolor enim non porro error nisi repellat excepturi quos doloribus inventore a. Cupiditate, qui consequatur.
-            Eum facilis eveniet odio sequi corrupti provident doloremque porro cupiditate dolores debitis ipsum sint laboriosam, magni accusantium, numquam aperiam commodi harum? Id maxime inventore itaque adipisci! Deleniti aliquam magni est?',
-            'photo' => 'meeting-photo/cafe2.avif'
-        ]);
+        // MeetingHistory::create([
+        //     'wedding_id' => 1,
+        //     'topic' => 'Pertemuan 2',
+        //     'meeting_date' => Carbon::now(),
+        //     'meeting_time' => Carbon::now(),
+        //     'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus ipsum, quisquam neque nobis molestiae quam qui numquam. Possimus harum nostrum iste architecto cumque distinctio, officia iure placeat minus et voluptas?
+        //     Iusto doloremque, ab nisi, sunt ipsam quidem sit laboriosam itaque hic neque tempora commodi facilis? Dolore nesciunt saepe maxime odio delectus? Accusantium nostrum libero odit, ea eaque earum necessitatibus rerum?
+        //     Odit iusto, voluptatem aspernatur ducimus reprehenderit aliquam harum quasi porro officia omnis nulla ut necessitatibus ea beatae consequuntur, quos, a sapiente cumque. Eos, laborum repellat praesentium impedit dolore dignissimos nemo.
+        //     Exercitationem repudiandae officia reprehenderit tenetur maiores fugit aliquam, quae quibusdam quod impedit enim sequi nostrum debitis, dolorem provident asperiores deleniti laboriosam. Similique quia saepe vel nam earum explicabo sed aperiam.
+        //     Quam rem minima fugiat. Facere inventore voluptate dolorum eius architecto nihil nulla iusto hic perferendis? Incidunt, ab illo! Ad libero quam voluptatem deserunt amet quo reprehenderit et molestiae autem suscipit?
+        //     Quam labore voluptatem illum illo consequuntur nesciunt eligendi quidem obcaecati praesentium nihil deleniti aliquam, eum ipsam neque beatae quia laborum dolores facilis. Enim laudantium nihil atque voluptatum mollitia libero aliquam.
+        //     Sapiente quaerat rerum nesciunt deserunt odit repudiandae minus, quos praesentium itaque ad laudantium corrupti nostrum expedita eveniet dolorum cumque fugiat reiciendis voluptate eligendi nemo doloremque earum ipsum. Voluptate, possimus nesciunt.
+        //     Doloremque deleniti beatae quae tempore aspernatur facere praesentium harum fugiat corporis eligendi dolores dolore provident corrupti nobis inventore non quo molestias ab eos, qui vel laborum debitis. Eaque, cupiditate nostrum.
+        //     Sint perspiciatis maiores iste sit harum eos voluptates, suscipit nesciunt tempora, ut nihil magni beatae dolor enim non porro error nisi repellat excepturi quos doloribus inventore a. Cupiditate, qui consequatur.
+        //     Eum facilis eveniet odio sequi corrupti provident doloremque porro cupiditate dolores debitis ipsum sint laboriosam, magni accusantium, numquam aperiam commodi harum? Id maxime inventore itaque adipisci! Deleniti aliquam magni est?',
+        //     'photo' => 'meeting-photo/cafe2.avif'
+        // ]);
 
-        Payment::create([
-            'wedding_id' => 1,
-            'name' => 'Pembayaran Vendor',
-            'nominal' => 75000000
-        ]);
+        // Payment::create([
+        //     'wedding_id' => 1,
+        //     'name' => 'Pembayaran Vendor',
+        //     'nominal' => 75000000
+        // ]);
 
         // Konsep
         Theme::create([
             'name' => 'Modern',
-            'thumbnail' => 'theme-thumbnail/modern.avif',
-            'description' => '',
+            'thumbnail' => 'theme-thumbnail/modern.jpg',
+            'description' => 'Pernikahan modern biasanya dipenuhi dengan garis-garis bersih, bentuk geometris, dan desain yang minimalis. Agar tidak terkesan datar, gunakan sesuatu yang membuat Anda menonjol seperti gaun pengantin dengan warna selain putih, dekorasi dengan warna yang lebih tegas.',
         ]);
 
         ThemeImage::create([
             'theme_id' => 1,
-            'image' => 'theme-images/1.png'
+            'image' => 'theme-images/1.jpg'
         ]);
         ThemeImage::create([
             'theme_id' => 1,
@@ -118,13 +119,26 @@ class DatabaseSeeder extends Seeder
         ]);
         ThemeImage::create([
             'theme_id' => 1,
-            'image' => 'theme-images/3.png'
+            'image' => 'theme-images/3.jpg'
         ]);
 
         Theme::create([
             'name' => 'Tradisional',
-            'thumbnail' => 'theme-thumbnail/traditional.avif',
-            'description' => '',
+            'thumbnail' => 'theme-thumbnail/traditional.jpg',
+            'description' => 'Jika Anda ingin pernikahan dengan konsep antik, cari inspirasi dari pernikahan-pernikahan dari beberapa dekade yang lalu. Salah satu cara termudah adalah mencari gaun pengantin dan riasan di zaman tersebut. Untuk dekor, Anda bisa menggunakan benda-benda antik, seperti pintu atau tempat duduk yang using atau mobil klasik seperti Porsche atau Volkswagen.',
+        ]);
+
+        ThemeImage::create([
+            'theme_id' => 2,
+            'image' => 'theme-images/4.jpg'
+        ]);
+        ThemeImage::create([
+            'theme_id' => 2,
+            'image' => 'theme-images/5.jpg'
+        ]);
+        ThemeImage::create([
+            'theme_id' => 2,
+            'image' => 'theme-images/6.jpg'
         ]);
 
         $dekorasi = VendorType::create([
@@ -162,20 +176,22 @@ class DatabaseSeeder extends Seeder
             "name" => "CLOVER",
             "price" => 45000000,
             "logo" => "vendor-logo/clover.jpg",
-            "description" => "",
+            "description" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi debitis quaerat suscipit adipisci harum quisquam? Culpa, enim neque excepturi amet asperiores error consequuntur voluptas tempora nam laboriosam laudantium reiciendis at.
+            Porro commodi reprehenderit consequatur veniam quisquam a provident fugiat, voluptate aut doloribus esse! Pariatur nihil distinctio eaque, soluta eum quidem eius maiores aperiam in. Labore eligendi commodi laboriosam eveniet mollitia.
+            Aperiam rerum repellat unde consectetur illo alias eveniet impedit possimus dolores, ipsam nostrum recusandae. Eaque cum commodi reiciendis, voluptatibus fuga accusamus fugiat sed corporis ea! Rem similique eveniet odio voluptate.",
         ]);
 
         VendorImage::create([
             'vendor_id' => 1,
-            'image' => 'vendor-images/1.png'
+            'image' => 'vendor-images/1.jpg'
         ]);
         VendorImage::create([
             'vendor_id' => 1,
-            'image' => 'vendor-images/2.png'
+            'image' => 'vendor-images/2.jpg'
         ]);
         VendorImage::create([
             'vendor_id' => 1,
-            'image' => 'vendor-images/3.png'
+            'image' => 'vendor-images/3.jpg'
         ]);
         Vendor::create([
             "vendor_type_id" => $dekorasi,
@@ -273,5 +289,30 @@ class DatabaseSeeder extends Seeder
             'owner_name' => 'Gusti',
             'pin' => '71263861823',
         ]);
+
+        Employee::create([
+            'name' => 'Muhammad Syafiq',
+            'position' => 'Designer',
+            'phone_number' => '087811223344',
+            'email' => 'syafiq@yahoo.com',
+            'photo' => 'employee-photo/1.jpg',
+        ]);
+
+        Employee::create([
+            'name' => 'Nurcholis',
+            'position' => 'Director',
+            'phone_number' => '087842312321',
+            'email' => 'nurcholis@yahoo.com',
+            'photo' => 'employee-photo/2.jpg',
+        ]);
+
+        Employee::create([
+            'name' => 'Gusti',
+            'position' => 'Wedding Management',
+            'phone_number' => '087842123212',
+            'email' => 'gusti@yahoo.com',
+            'photo' => 'employee-photo/3.jpg',
+        ]);
+
     }
 }
